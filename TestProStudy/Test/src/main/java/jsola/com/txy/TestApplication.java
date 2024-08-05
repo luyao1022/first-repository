6491dc1edf691a1d4d63c3a27abe5be817144693
luyao <luyao10220917@gmail.com>
2024-08-05 11:07:52 +0800

    提交idea项目

---
+++ TestProStudy/Test/src/main/java/jsola/com/txy/TestApplication.java
@@ -0,0 +1,13 @@
+package jsola.com.txy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
---
+++ TestProStudy/Test/src/test/java/jsola/com/txy/TestApplicationTests.java
@@ -0,0 +1,17 @@
+package jsola.com.txy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void test(){
+        System.out.println("用于测试git");
+    }
+}
